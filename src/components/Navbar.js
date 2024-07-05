4e72017ff895067305f56c82469a8e5cf6ffd8bf
Vychann <thaovy4791@gmail.com>
2024-07-05 16:24:58 +0700

    nghi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+
 const Navbar = ({ username, cartItemCount }) => {
   return (
     <nav className="navbar">
       <Link to="/">Home</Link>
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
       {username ? (
         <span>Welcome, {username}</span>
       ) : (
